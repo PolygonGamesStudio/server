d88cd07932b224eede439db4deb5bbf35073b5ec
maratkanov <alexwitcher@gmail.com>
2014-04-06 22:14:35 +0400

    GameSession test 177/310

--- src/test/GameMechanicsTests/GameSessionTest.java
+++ src/test/GameMechanicsTests/GameSessionTest.java
@@ -30,4 +30,73 @@ public class GameSessionTest {
     public void whiteFirstTest() {
         Assert.assertEquals(gameSession.getNext(),'w');
     }
+
+    @Test
+    public void blackSecondTest() {
+        Assert.assertTrue(gameSession.checkStroke(id1,6,5,7,4));
+        Assert.assertEquals(gameSession.getNext(),'b');
+    }
+
+    @Test
+    public void makeWhitePawnTheKingTest() {
+        gameSession.checkStroke(id1,6,5,7,4);
+        gameSession.checkStroke(id2,2,5,3,4);
+        gameSession.checkStroke(id1,5,6,6,5);
+        gameSession.checkStroke(id2,3,6,2,5);
+        gameSession.checkStroke(id1,0,5,1,4);
+        gameSession.checkStroke(id2,4,7,3,6);
+        gameSession.checkStroke(id1,6,7,5,6);
+        gameSession.checkStroke(id2,2,5,1,4);
+        gameSession.checkStroke(id1,7,4,5,2);   // срублено две фигуры подряд
+        gameSession.checkStroke(id1,5,2,3,0);   // срублено две фигуры подряд
+        Assert.assertEquals(gameSession.getWhiteQuantity(), 12); // все белые фигуры целы
+        Assert.assertEquals(gameSession.getBlackQuantity(), 10); // у чёрных съедено 2 фигуры
+    }
+
+    @Test
+    public void makeBlackKingAndBeatTest() {
+        gameSession.checkStroke(id1,4,5,5,4);
+        gameSession.checkStroke(id2,4,5,5,4);
+        gameSession.checkStroke(id1,5,6,4,5);
+        gameSession.checkStroke(id2,2,5,1,4);
+        gameSession.checkStroke(id1,6,7,5,6);
+        gameSession.checkStroke(id2,1,6,2,5);
+        gameSession.checkStroke(id1,4,5,3,4);
+        gameSession.checkStroke(id2,5,4,3,2);
+        gameSession.checkStroke(id2,3,2,1,0);
+        gameSession.checkStroke(id1,4,7,5,6);
+        gameSession.checkStroke(id2,1,0,5,4);
+
+        Assert.assertEquals(gameSession.getWhiteQuantity(), 9); // все белые фигуры целы
+        Assert.assertEquals(gameSession.getBlackQuantity(), 12); // у чёрных съедено 2 фигуры
+    }
+
+    @Test
+    public void KingHasBeenEaten() {
+        gameSession.checkStroke(id1,0,5,1,4);
+        gameSession.checkStroke(id2,2,5,3,4);
+        gameSession.checkStroke(id1,1,4,0,3);
+        gameSession.checkStroke(id2,1,6,2,5);
+        gameSession.checkStroke(id1,1,6,0,5);
+        gameSession.checkStroke(id2,0,7,1,6);
+        gameSession.checkStroke(id1,2,5,3,4);
+        gameSession.checkStroke(id2,3,4,5,2);
+        gameSession.checkStroke(id1,3,6,1,4);
+        gameSession.checkStroke(id2,2,5,3,4);
+        gameSession.checkStroke(id1,4,7,3,6);
+        gameSession.checkStroke(id2,0,5,1,4);
+        gameSession.checkStroke(id1,3,6,2,5);
+        gameSession.checkStroke(id2,1,4,0,3);
+        gameSession.checkStroke(id1,4,5,3,4);
+        gameSession.checkStroke(id2,6,5,5,4);
+        gameSession.checkStroke(id1,3,4,5,2);
+        gameSession.checkStroke(id1,5,2,7,0);
+        gameSession.checkStroke(id2,4,5,3,4);
+        gameSession.checkStroke(id1,7,0,3,4);
+        gameSession.checkStroke(id1,3,4,1,2);
+        gameSession.checkStroke(id2,7,6,5,4);
+
+        Assert.assertEquals(gameSession.getWhiteQuantity(), 10); // все белые фигуры целы
+        Assert.assertEquals(gameSession.getBlackQuantity(), 7); // у чёрных съедено 2 фигуры
+    }
 }
